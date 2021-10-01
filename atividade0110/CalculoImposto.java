8e0d268ac82076f6980bb4b750185d3fc2e56739
thcarval <contatothalitanunes@gmail.com>
2021-10-01 15:07:13 -0300

    exercicio finalizado

--- atividade0110/CalculoImposto.java
+++ atividade0110/CalculoImposto.java
@@ -1,8 +1,8 @@
 /*
-_ Execute o commit e push a cada etapa finalizada.
-_ Crie uma classe para cálculo de impostos.
-_ A classe de calculo deve possuir 3 métodos, um para calculo de ISS, outro para cálculo de IOF e outro para cálculo de IR.
-_ A classe deve manter em variáveis privadas o valor de cada imposto que deve ser definido por você.
+_ Execute o commit e push a cada etapa finalizada. OK
+_ Crie uma classe para cálculo de impostos. OK
+_ A classe de calculo deve possuir 3 métodos, um para calculo de ISS, outro para cálculo de IOF e outro para cálculo de IR.OK
+_ A classe deve manter em variáveis privadas o valor de cada imposto que deve ser definido por você. OK
 _ Os métodos devem receber como parâmetro um valor que servirá de base para cálculo do imposto e retornar o valor do imposto calculado.
 _ Crie uma segunda classe que contenha o método main.
 _ Nesta segunda classe, crie um objeto da classe calculadora, realize a chamada aos três métodos e ao final da execução, imprima o valor de todos os impostos calculados,.
@@ -13,22 +13,22 @@ package atividade0110;
 
 public class CalculoImposto {
 
-    private double valor = 0.0;
+    private double valor = 2000;
     private double iss = 0.04;
     private double iof = 0.0638;
     private double ir = 0.09;
 
-    double metodoIss(double valor, double iss){
+    double metodoIss(){
         double resultado = valor * iss;
         return resultado;
     }
 
-    double metodoIof(double valor, double iof){
+    double metodoIof(){
         double resultado = valor * iof;
         return resultado;
     }
 
-    double metodoIr(double valor, double ir){
+    double metodoIr(){
         double resultado = valor * ir;
         return resultado;
     }
--- atividade0110/CalculoImpostoMain.java
+++ atividade0110/CalculoImpostoMain.java
@@ -2,6 +2,12 @@ package atividade0110;
 
 public class CalculoImpostoMain {
     public static void main(String[] args) {
-        
+        CalculoImposto calc = new CalculoImposto();
+
+        double iss = calc.metodoIss();
+        double iof = calc.metodoIof();
+        double ir = calc.metodoIr();
+
+        System.out.printf("O valor dos impostos são: \n == Iss == %.2f \n == Iof == %.2f \n == IR == %.2f \n Goodbye!", iss, iof, ir);
     }
 }
