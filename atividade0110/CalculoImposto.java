3be831536b17347948d6463b6dff7be746866455
thcarval <contatothalitanunes@gmail.com>
2021-10-01 15:16:54 -0300

    retirei o valor estático

--- atividade0110/CalculoImposto.java
+++ atividade0110/CalculoImposto.java
@@ -3,9 +3,9 @@ _ Execute o commit e push a cada etapa finalizada. OK
 _ Crie uma classe para cálculo de impostos. OK
 _ A classe de calculo deve possuir 3 métodos, um para calculo de ISS, outro para cálculo de IOF e outro para cálculo de IR.OK
 _ A classe deve manter em variáveis privadas o valor de cada imposto que deve ser definido por você. OK
-_ Os métodos devem receber como parâmetro um valor que servirá de base para cálculo do imposto e retornar o valor do imposto calculado.
-_ Crie uma segunda classe que contenha o método main.
-_ Nesta segunda classe, crie um objeto da classe calculadora, realize a chamada aos três métodos e ao final da execução, imprima o valor de todos os impostos calculados,.
+_ Os métodos devem receber como parâmetro um valor que servirá de base para cálculo do imposto e retornar o valor do imposto calculado. ok
+_ Crie uma segunda classe que contenha o método main. OK
+_ Nesta segunda classe, crie um objeto da classe calculadora, realize a chamada aos três métodos e ao final da execução, imprima o valor de todos os impostos calculados,.OK
 
 */
 
@@ -13,7 +13,6 @@ package atividade0110;
 
 public class CalculoImposto {
 
-    private double valor = 2000;
     private double iss = 0.04;
     private double iof = 0.0638;
     private double ir = 0.09;
