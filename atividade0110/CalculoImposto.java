83d946bd9605198969cee0f037c91e579db9a0fb
thcarval <contatothalitanunes@gmail.com>
2021-10-01 14:39:59 -0300

    criacao classe calculo de impostos

---
+++ atividade0110/CalculoImposto.java
@@ -0,0 +1,16 @@
+/*
+_ Execute o commit e push a cada etapa finalizada.
+_ Crie uma classe para cálculo de impostos.
+_ A classe de calculo deve possuir 3 métodos, um para calculo de ISS, outro para cálculo de IOF e outro para cálculo de IR.
+_ A classe deve manter em variáveis privadas o valor de cada imposto que deve ser definido por você.
+_ Os métodos devem receber como parâmetro um valor que servirá de base para cálculo do imposto e retornar o valor do imposto calculado.
+_ Crie uma segunda classe que contenha o método main.
+_ Nesta segunda classe, crie um objeto da classe calculadora, realize a chamada aos três métodos e ao final da execução, imprima o valor de todos os impostos calculados,.
+
+*/
+
+package atividade0110;
+
+public class CalculoImposto {
+    
+}
