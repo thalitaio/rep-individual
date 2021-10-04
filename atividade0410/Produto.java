ca045fa32a8bc84058edf7f76aa960ce4190f2ee
thcarval <contatothalitanunes@gmail.com>
2021-10-04 13:20:37 -0300

    Criacao classe Produto, criacao atributos classe

---
+++ atividade0410/Produto.java
@@ -0,0 +1,25 @@
+/*
+_ Crie uma classe produto com os atributos Código, Nome, Descrição e Valor. OK
+_ Todos os atributos da classe produtos devem ser privados. OK
+_ Construa os métodos get e set para cada um dos atributos.
+_ Crie uma classe Conta Corrente com os atributos Agência, Numero, Codigo do Cliente e Saldo.
+_ Todos os atributos da classe Conta Corrente devem ser privados.
+_ Construa os métodos get e set para cada um dos atributos da classe.
+_ Crie uma classe Tela Principal Loja que contenha um método main.
+_ Nesta classe, crie um objeto da Produto, realize a chamada a todos os métodos set e get, ao final da execução, imprima o valor de todos os atributos da classe.
+_ Crie uma classe Tela Principal Banco que contenha um método main.
+_ Nesta classe, crie um objeto da Conta Corrente, realize a chamada a todos os métodos set e get, ao final da execução, imprima o valor de todos os atributos da classe.
+
+
+*/
+
+
+
+package atividade0410;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private boolean valor;
+}
