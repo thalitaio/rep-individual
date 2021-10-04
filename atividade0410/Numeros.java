c9489211112267bf22aa63b6f1a8715adffb51ec
thcarval <contatothalitanunes@gmail.com>
2021-10-04 17:12:13 -0300

    Classe numeros com metodo main criada

---
+++ atividade0410/Numeros.java
@@ -0,0 +1,25 @@
+/*
+
+_ Utilize o repositório criado durante a realização da sua última atividade individual.
+_ Execute o commit e push a cada etapa finalizada.
+_ Crie uma classe Números com um método main.
+_ Crie nesta classe um array de números inteiros com 10 posições.
+_ Adicione um valor para cada posição do array.
+_ Imprima ao final da execução do main, todas as posições do array utilizando o laço de repetição for.
+_ Crie uma classe Pessoa Física com os atributos código, nome, sobrenome, idade, rg e cpf.
+_ Todos os atributos da classe Pessoa Física devem ser privados.
+_ Construa os métodos get e set para cada um dos atributos da classe.
+_ Crie uma classe Cadastro de Pessoas que contenha um método main.
+_ Nesta classe, crie um array de Pessoa com 5 posições.
+_ Crie 5 objetos da classe pessoa, chame os método set para atribuir valores diferente para cada pessoa e salve cada objeto em uma posição do array.
+_ Imprima todos os dados das 5 pessoas utilizando o laço de repetição foreach.
+
+
+*/
+
+
+package atividade0410;
+
+public class Numeros {
+    
+}
