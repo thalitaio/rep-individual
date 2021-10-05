05369ced2988c8bbefa7b9f4fe478f2a3046f412
thcarval <contatothalitanunes@gmail.com>
2021-10-05 15:23:42 -0300

    classe pessoa e propriedades criadas

---
+++ atividade0510/Pessoa.java
@@ -0,0 +1,25 @@
+/*
+_ Crie uma classe Pessoa com as propriedades nome, sobrenome e matrícula.
+_ Crie uma classe Funcionário com as propriedades salário e departamento.
+_ Crie uma classe Coordenador com as propriedades Curso Coordenado e bônus.
+_ Crie uma classe Aluno com as propriedades curso e turma.
+_ Altere a classe Funcionario para que ela herde da classe Pessoa.
+_ Altere a classe Coordenador para que ela herde da classe Funcionário.
+_ Altere a classe Aluno para que ela herde da classe Pessoa.
+_ Os atributos de todas as classes devem ser privados.
+_ Construa os métodos get e set para cada um dos atributos das classes.
+
+_ Crie uma classe Universidade, adicione um método main, crie um objeto de Funcionario, um objeto de Coordenador e um objeto de Aluno.
+_ Utilize os métodos set dos objetos de Funcionario, Coordenador e Aluno para inserir valores.
+_ Imprima todos os dados dos objetos, utilizando seu metodos get, ao final da execução.
+
+*/
+
+
+package atividade0510;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+    private String matricula;
+}
