33af18935457645fc7f7748caf45b5507580afad
thcarval <contatothalitanunes@gmail.com>
2021-10-07 17:29:31 -0300

    exercicio finalizado

--- atividade0710/Calculadora.java
+++ atividade0710/Calculadora.java
@@ -9,5 +9,19 @@ _ Crie uma classe Tela com um método main, crie um objeto da classe Calculadora
 package atividade0710;
 
 public class Calculadora {
-    
+    public int soma( int a, int b) {
+        return a + b;
+    }
+
+    public double soma(double a, double b) {
+        return a + b;
+    }
+
+    public float soma(float a, float b, float c) {
+        return a + b + c;
+    }
+
+    public long soma(long a, long b, long c){
+        return a + b + c;
+    }
 }
---
+++ atividade0710/TelaCalc.java
@@ -0,0 +1,22 @@
+/*
+ Crie uma classe Tela com um método main, crie um objeto da classe Calculadora, chame cada método criado e imprima seus resultados.
+*/
+
+package atividade0710;
+
+public class TelaCalc {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        double c1 = calc.soma(6.5, 6);
+        float c3 = calc.soma(10.3f, 4.3f, 8.5f);
+        double c2 = calc.soma(10, 11);
+        long c4 = calc.soma(105, 45, 33);
+
+       System.out.println(c1);
+       System.out.println(c2);
+       System.out.println(c3);
+       System.out.println(c4);
+
+    }
+}
