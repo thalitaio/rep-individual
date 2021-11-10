12a0f97a86192a26c97d001ad4b043369afa5232
thcarval <contatothalitanunes@gmail.com>
2021-11-10 17:29:45 -0300

    up

--- atividade1011/CriacaoTabelas.sql
+++ atividade1011/CriacaoTabelas.sql
@@ -60,3 +60,102 @@ alter table produto
 add constraint fk_produto foreign key(catID) references categoria(id);
 
 
+-- Crie uma ScriptsSQL em um repositório Git e salve os scripts criados nesta atividade 
+-- dentro desta pasta. Envie as alterações para o GitHub através dos comandos GIT. 
+-- No GitHub, adicione o usuário maykondgranemann, como colaborador.
+
+-- Exercício 42
+
+-- Insira 3 categorias na tabela categoria
+insert into categoria (
+	nome
+	,descricao 
+)
+values(
+	'nome3'
+	,'sobrenome3'
+);
+
+--Insira 6 produtos na tabela produto.
+select * from produto;
+insert into produto(
+	nome 
+	,descricao 
+	,preco
+	,catid 
+)
+values(
+	'prod6'
+	,'prod6 descricao'
+	,40.00	
+	,7
+);
+
+-- Altere a descrição de dois produtos.
+update produto 
+	set
+		descricao = 'nova descricao 1'
+	where nome = 'prod2';
+
+-- Delete um produto e uma categoria
+delete from produto 
+where nome = 'prod4';
+
+delete from categoria 
+where nome = 'nome2';
+
+-- Altere uma categoria de um produto.
+ALTER TABLE produto RENAME COLUMN nome TO nomezaum;
+
+-- Altere o preço de todos os produtos tirando R$1,00 do preço atual.
+update produto 
+	set
+		preco = preco - 1;
+	
+-- Liste todos os dados de todas as colunas da tabela Categoria.
+-- Liste todos os dados de todas as colunas da tabela Produto.
+select * from categoria c ;
+select * from produto p ;
+
+-- Liste os dados de todas as colunas da tabela Categoria, 
+-- para as linhas que possuam uma descrição preenchida.
+select * from categoria c 
+where (descricao  is not null);
+
+-- Liste os dados das colunas Id, nome e preço da tabela Produto, 
+-- para as linhas que possuem um nome iniciando com C.
+select 
+	p.id
+	,p.nomezaum
+	,p.preco
+from produto p where nomezaum = 'c%';
+
+-- Liste os dados das colunas Id, nome e preço da tabela Produto e os 
+-- dados id, nome e descrição da tabela Categoria para as linhas que 
+-- possuem um preço maior que 0.
+
+select 
+	p.id
+	,p.nomezaum
+	,p.preco 
+	,c.id 
+	,c.nome
+	,c.descricao 
+from produto p 
+join categoria c on p.preco > 0;
+
+-- Liste os dados de todas as colunas da tabela Produto que possuem categoria com Id maior que 2.
+
+select * 
+from produto p where catid > 2;
+
+-- Liste os dados de todas as colunas da tabela Categoria e o 
+-- os dados da coluna nome da tabela Produto, para as categorias 
+-- que estão sendo utilizadas por produtos.
+
+select * 
+from categoria c
+join produto p on c.nome where c.id = p.catid;
+
+
+
